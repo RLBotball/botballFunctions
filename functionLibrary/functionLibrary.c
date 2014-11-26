23520ba920210823461fcfd4bf4da5a2ab5ce4c7
InfinityDeltaX <delta0357@gmail.com>
2014-11-26 14:25:58 -0500

    Remembered C
    Made function skeletons compile.

--- functionLibrary/functionLibrary.c
+++ functionLibrary/functionLibrary.c
@@ -1,3 +1,30 @@
+#define VAR 10
+
 int main(){
+	printf(VAR+" ");
+	return(0);
+}
+
+/*
+1 dist unit = 1 second at speed 100 with standard wheels;
+*/
+
+int findValidObject(){ //find an object for the purpose of tracking. Should not move.
+	
+}
+
+void driveStraightWithTracking(int dist){
+	
+}
+
+int moveInCM(int cm){
+	
+}
+
+int turnLeft(int deg){ //perform a turn of deg degrees.
+	
+}
+
+int turnRight(int deg){
 	
 }
