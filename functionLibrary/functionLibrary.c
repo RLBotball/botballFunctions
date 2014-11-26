32668860ca98c3b798c1854fddc3ee7ff778970b
InfinityDeltaX <delta0357@gmail.com>
2014-11-26 15:54:25 -0500

    Implemented Degree Turns
    Implemented TurnRightDeg() and turnLeftDeg().

--- functionLibrary/functionLibrary.c
+++ functionLibrary/functionLibrary.c
@@ -1,10 +1,12 @@
-#define RIGHT_MOTOR_PORT 0
-#define LEFT_MOTOR_PORT 2
+#define RIGHT_MOTOR_PORT 2
+#define LEFT_MOTOR_PORT 0
+#define STANDARD_TURN_SPEED 200
 
+static int unitsPer360Turn = 4700000; //this number of units will makes the robot turn 360 degrees. Calculate with testTurnSpeed().
 
 int main(){
 	//printf(VAR+" ");
-	testTurnSpeed();
+	turnLeftDeg(180);
 	return(0);
 }
 
@@ -16,23 +18,24 @@ void setup(){ //deal with initializing everything
 1 dist unit = 1 second at speed 100 with standard wheels;
 */
 int testTurnSpeed(){ //account for the distance between the wheels, etc. Returns a "dist" number which represents a 360 degree turn. Speed of turn * length of turn = value returned. 
+	//Thus, it takes X number of units (X determined here), to turn 360 degrees.
 	int currentPosition = 0;
-	int SPEED = 250;
+	int FIRST_SPEED = 250; //speed at which for the first portion.
 	int TIMEINTERVAL = 40;
 	printf("Please press A when the Robot has turned 90 degrees. \n");
 	while(a_button_clicked()==0){ //turn a bit past 90 degrees.
-		turnRight(SPEED, TIMEINTERVAL);
-		currentPosition = currentPosition + (SPEED*TIMEINTERVAL);
+		turnRight(FIRST_SPEED, TIMEINTERVAL);
+		currentPosition = currentPosition + (FIRST_SPEED*TIMEINTERVAL);
 	}
 	
 	printf("Moving back a bit. \n");
 	int toMoveBack = currentPosition/5; //move back 1/5 of the distance.
-	turnLeft(SPEED, toMoveBack/SPEED);
+	turnLeft(FIRST_SPEED, toMoveBack/FIRST_SPEED);
 	
 	printf("Please press A when the Robot is back at 90 degrees. \n");
 	while(a_button_clicked()==0){ //turn to 90 degrees at 1/4 previous speed.
-		turnRight(SPEED/4, TIMEINTERVAL);
-		currentPosition = currentPosition + (SPEED/4*TIMEINTERVAL);
+		turnRight(FIRST_SPEED/4, TIMEINTERVAL);
+		currentPosition = currentPosition + (FIRST_SPEED/4*TIMEINTERVAL);
 	}
 	
 	printf("Current Value: %d \n Will now turn 360 degrees. \n", currentPosition*4);
@@ -42,7 +45,7 @@ int testTurnSpeed(){ //account for the distance between the wheels, etc. Returns
 	printf("Button A: Done \n Button B: Redo \n");
 	int button = getFirstButtonPressed();
 	if(button == 0){
-		printf("360 degree turn value: %d \n", currentPosition*4);
+		printf("360 degree turn value: %d \n", (currentPosition*4));
 	} else if(button == 1){
 		return testTurnSpeed();
 	} else return -1;
@@ -64,7 +67,7 @@ int getFirstButtonPressed(){ //returns 0 for A, 1 for B, 2 for C.
 	}
 }
 
-int turnRight(int speed, int time){
+void turnRight(int speed, int time){
 	mav(RIGHT_MOTOR_PORT, speed);
 	mav(LEFT_MOTOR_PORT, (speed*-1));
 	msleep(time);
@@ -72,7 +75,7 @@ int turnRight(int speed, int time){
 	mav(RIGHT_MOTOR_PORT, 0);
 }
 
-int turnLeft(int speed, int time){
+void turnLeft(int speed, int time){
 	mav(LEFT_MOTOR_PORT, speed);
 	mav(RIGHT_MOTOR_PORT, (speed*-1));
 	msleep(time);
@@ -93,9 +96,17 @@ int moveInCM(int cm){
 }
 
 int turnLeftDeg(int deg){ //perform a turn of deg degrees.
-	
+	if(unitsPer360Turn == -1){printf("ERROR: Define unitsPer360Turn! \n");}
+	else{
+		float percentTurn = (float)deg/360;
+		turnLeft(STANDARD_TURN_SPEED, (unitsPer360Turn*percentTurn)/STANDARD_TURN_SPEED);//unitsPer360Turn:360 as ? : deg
+	}
 }
 
 int turnRightDeg(int deg){
-	
+		if(unitsPer360Turn == -1){printf("ERROR: Define unitsPer360Turn! \n");}
+	else{
+		float percentTurn = (float)deg/360;
+		turnRight(STANDARD_TURN_SPEED, (unitsPer360Turn*percentTurn)/STANDARD_TURN_SPEED);//unitsPer360Turn:360 as ? : deg
+	}
 }
